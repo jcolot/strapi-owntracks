ec0543e24d24dba2735fd9413b6a171f7a8b322f
jcolot <julien.colot@gmail.com>
2022-09-26 21:02:49 +0000

    Update model

--- src/api/track/controllers/track.js
+++ src/api/track/controllers/track.js
@@ -11,9 +11,10 @@ module.exports = createCoreController('api::track.track', ({ strapi }) => ({
   async create(ctx) {
     const unparsed = require("koa-body/unparsed.js");
     const body = ctx.request.body;
-    const unparsedBody = ctx.request.body[unparsed];
+    const unparsedBody = JSON.parse(ctx.request.body[unparsed]);
+    unparsedBody.dt = new Date(unparsedBody.tst * 1000).toISOString();
     const entity = await strapi.entityService.create('api::track.track', {
-      data: JSON.parse(unparsedBody), 
+      data: unparsedBody, 
     });
     const sanitizedEntity = await sanitize.contentAPI.output(entity);
     return { data: sanitizedEntity };
